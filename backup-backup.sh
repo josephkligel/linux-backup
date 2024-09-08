ea6afdb5d4ae7017b5ddfc2d256623639059240e
Joseph Kligel <jkligel@gmail.com>
2024-09-07 19:13:22 -0500

    Using GitProject program to push

--- backup-backup.sh
+++ backup-backup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
-$DD=/media/joseph/My2TB
+$DD="/media/joseph/My2TB"
 
-$BP=/media/joseph/Backup_Plus
+$BP="/media/joseph/Backup_Plus"
 
 function backup_backup {
-	rsync -auv "$BP/*" "$DD"
+	rsync -auv $BP/ $DD
 }
 
 backup_backup
